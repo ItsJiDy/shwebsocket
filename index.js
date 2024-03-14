6912e6563a5a41255525c3059406710f93c04f7d
ItsJiDy <96864066+ItsJiDy@users.noreply.github.com>
2024-03-14 08:45:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,75 @@ const Server = new WebSocket.Server({
 const GlobalChat = []
 var ServerStatus = "Online"
 
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
+App.get(
+    '/spotify/generate_token/:basicToken',
+    (Request, Response) => {
+        async function generate(token) {
+            const response = await fetch(`https://accounts.spotify.com/api/token?grant_type=client_credentials`, {
+                method: 'POST',
+                headers: {
+                  'Authorization': 'Basic ' + token,
+                  'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            });
+            const data = await response.text();
+            Response.send(data);
+        }
+        generate(Request.params.basicToken)
+    }
+)
+
+App.get(
+    '/isalive',
+    (Request, Response) => {
+        Response.send("Yes alive!")
+    }
+)
+
+App.get(
+    '/spotify/searchTracks/:access_token/:query',
+    (Request, Response) => {
+        async function searchTracks(query, access_token) {
+            const response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${query}`, {
+                headers: {
+                  'Authorization': 'Bearer ' + access_token
+                }
+            });
+            const data = await response.text();
+            Response.send(data);
+        }
+        searchTracks(Request.params.query, Request.params.access_token);
+    }
+)
+
+App.get(
+    '/spotify/callback',
+    (Request, Response) => {
+        Response.send(Request.query.access_token)
+    }
+)
+
+App.get(
+    '/spotify/login',
+    (Request, Response) => {
+        var scope = 'user-read-private user-read-email playlist-read-private';
+
+        Response.redirect('https://accounts.spotify.com/authorize?response_type=token&client_id=de1fcde34f21465091a7cda4b756ad8e&scope=' + scope + '&redirect_uri=http://localhost:3000/spotify/callback&state=' + makeid(16))
+    }
+)
+
 App.get(
     '/script/shutdown/status',
     (Request, Response) => {
